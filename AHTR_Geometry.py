94b87daa85ebd07950c1a64e9640ac6c2408f775
sierratutwiler <tutwilersa@vcu.edu>
2020-07-07 17:46:30 -0400

    Added additional parameters

--- AHTR_Geometry.py
+++ AHTR_Geometry.py
@@ -1,11 +1,13 @@
 import numpy as np 
-#Notes: 324 fuel assemblies
+#Notes: 324 fuel assembly columns
 
 Hc = 7.9 #Active height of core - m
 Ar = 1.48 #Active radius of core - m
 NFuel = 216*324 #Number of fuel rods
 Cr = (0.9525/100)/2 #Radius of coolant channel holes - m
 CVol = 1260 #Volume of the core - m^3
+Qth = 2400*(10**6) #Core thermal production - W
+Uinlet = 2.3 #Inlet velocity of coolant - m/s
 
 def A(Cr):
 	A = np.pi*(Cr**2) #Cross sectional area of coolant hole - m^2
