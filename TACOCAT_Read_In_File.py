8ccd280b035b853a6fbede69e18d66d0ba582b7a
sierratutwiler <tutwilersa@vcu.edu>
2020-07-19 13:36:13 -0400

    Read in file for TACOCAT

---
+++ TACOCAT_Read_In_File.py
@@ -0,0 +1,25 @@
+#TACOCAT read in file
+
+#Provide chemical composition of fuel. Fuel types include: U_Zr10, UC, UO2, PuO2, ThO2, UN, U3Si2, MOX, U
+Fuel_Type = "UC"
+
+#Provide chemical composition of coolant. Coolant types include: NaK, FLiBe, FLiNaK, NaF_ZrF4
+Coolant = "NaK"
+
+#Provide reactor geometry. Geometry types include: 
+Geometry = ""
+
+
+'''
+Example input:
+
+#Provide chemical composition of fuel. Fuel types include: U_Zr10, UC, UO2, PuO2, ThO2, UN, U3Si2, MOX, U
+Fuel_Type = "UC"
+
+#Provide chemical composition of coolant. Coolant types include: NaK, FLiBe, FLiNaK, NaF_ZrF4
+Coolant = "NaK"
+
+#Provide reactor geometry. Geometry types include: 
+Geometry = ""
+
+'''
