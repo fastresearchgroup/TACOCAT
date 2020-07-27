e5fa5ded4f2d4103f1772dabc83fbc6fee5a14db
sierratutwiler <tutwilersa@vcu.edu>
2020-07-27 17:33:39 -0400

    Added reactor name and general core parameters

--- TACOCAT_Read_In_File.py
+++ TACOCAT_Read_In_File.py
@@ -1,5 +1,8 @@
 #TACOCAT read in file
 
+#Provide the name of the reactor
+Reactor_Title = "Lunar_Reactor_689"
+
 #Provide chemical composition of fuel. Fuel types include: U_Zr10, UC, UO2, PuO2, ThO2, UN, U3Si2, MOX, U
 Fuel_Type = "UC"
 
@@ -9,7 +12,8 @@ Coolant = "NaK"
 #Provide reactor geometry. Geometry types include: 
 Geometry = ""
 
-
+Hc = 0.35 #Active Height of Core is 2m
+Qth = 3*10**6 #Core Thermal Production - W
 '''
 Example input:
 
