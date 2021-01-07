c96874f8e69cd041d5dbd1517433f4c854d86a47
sierratutwiler <tutwilersa@vcu.edu>
2021-01-07 11:08:13 -0500

    Moved fuel references from TACOCAT main to fuel module

--- Fuel_Props.py
+++ Fuel_Props.py
@@ -1,3 +1,10 @@
+#Fuel properties for TACOCAT calculations
+#References:
+	#Fuel:
+		#UC: Manara, D., De Bruycker, F., Boboridis, K., Tougait, O., Eloirdi, R., & Malki, M. (2012). High temperature radiance spectroscopy measurements of solid and liquid uranium and plutonium carbides. Journal of Nuclear Materials, 1–3, 126–138.
+		#U-Zr10: L. B. CARASIK, P. O’NEAL, M. KENNINGTON, T. HUGHES, H. HONANG, N, GOTH, A 3 MW NaK Cooled Fast Reactor for Use as the First Lunar Base Fission Power Unit, Technical Report, 2015.
+		#All others: Todreas, N., Kazimi, M. (2012). Nuclear Systems: Thermal Hydraulic Fundamentals.
+
 U_Zr10 = {
 	"kfuel": 22, #Thermal Conductivity of Fuel - W/m-C @ 1000 C
 	"Tmelt": 1160.00 #Melting temperature - C
