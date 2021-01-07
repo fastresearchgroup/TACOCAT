f68346d8496c714e861367f16946b4facb79d1c4
sierratutwiler <tutwilersa@vcu.edu>
2021-01-07 10:55:48 -0500

    Moved fuel properties from TACOCAT main file to a separate module.

---
+++ Fuel_Props.py
@@ -0,0 +1,57 @@
+U_Zr10 = {
+	"kfuel": 22, #Thermal Conductivity of Fuel - W/m-C @ 1000 C
+	"Tmelt": 1160.00 #Melting temperature - C
+}
+
+UC = {
+	"kfuel": 23, #Thermal Conductivity of Fuel - W/m-C
+	"Tmelt": 2506.85 #Melting temperature - C (2780 K)
+}
+
+UO2 = {
+	"kfuel": 3.6, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 2800 #Melting Temperature - C
+}
+
+PuO2 = {
+	"kfuel": 4.3, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 2374 #Melting Temperature - C
+}
+
+ThO2 = {
+	"kfuel": 5.76, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 3378 #Melting Temperature - C
+}
+
+UN = {
+	"kfuel": 21, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 2800 #Melting Temperature - C
+}
+
+U3Si2 = {
+	"kfuel": 15, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 1665 #Melting Temperature - C
+}
+
+MOX = {
+	"kfuel": 3.7, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 2774 #Melting Temperature - C
+	#MOX values are for 94% UO2 and 6% PuO2
+}
+
+U = {
+	"kfuel": 32, #Thermal Conductivity of fuel averaged between 200 C and 1000 C - W/m-C
+	"Tmelt": 1133 #Melting Temperature - C
+}
+
+Fuel_props = {
+	"U_Zr10": U_Zr10,
+	"UC": UC,
+	"UO2": UO2,
+	"PuO2": PuO2,
+	"ThO2": ThO2,
+	"UN": UN,
+	"U3Si2": U3Si2,
+	"MOX": MOX,
+	"U": U
+}
