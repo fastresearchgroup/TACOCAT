1811ac087bfbb2e8ae7d85da6baf8a1a50259ef5
Trevor <franklint2@vcu.edu>
2022-08-30 18:54:35 -0400

    removed Z from TACOCAT and added to geometry file

--- TACOCAT/src/Geometry_Value.py
+++ TACOCAT/src/Geometry_Value.py
@@ -8,6 +8,7 @@ import numpy as np
 #---------------------------------------------------------------------------------------#
 # Geometry - Core
 steps = 36
+z = np.linspace(-Hc/2,Hc/2,steps) #this needs to be a numpy array of position along the core in - m
 Ar = 33.8/100 #Active Radius of the core - m
 Ac = (2*(Ar**2)*np.pi/(3*3**0.5))**0.5 #Length of Hexagonal Size - m
 
