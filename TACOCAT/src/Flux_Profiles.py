fc86b48ed91ffa2a4e71ece75dd78737abd2feb5
Trevor <franklint2@vcu.edu>
2022-09-12 11:42:51 -0400

    removed ploting from flux cases

--- TACOCAT/src/Flux_Profiles.py
+++ TACOCAT/src/Flux_Profiles.py
@@ -16,32 +16,19 @@ k = 1
 #Flat Line Flux Profile (x = 1)
 FlatLineFlux = np.zeros(steps)
 FlatLineFlux[:] = 1
-plt.figure(k)
-plt.plot(z,FlatLineFlux, 'k-')
-plt.grid()
-k = k + 1
+
 
 #Linear Flux Profile (y=x)
 LinearFlux = np.zeros(steps)
 LinearFlux[:] = z[:]
-plt.figure(k)
-plt.plot(z,LinearFlux, 'k-')
-plt.grid()
-k = k + 1
+
 
 #Exponential Flux Profile
 ExponentialFlux = np.zeros(steps)
 ExponentialFlux[:] = np.exp(z)
-plt.figure(k)
-plt.plot(z,ExponentialFlux, 'k-')
-plt.grid()
-k = k + 1
+
 
 #Cosine Flux Profile
 CosineFlux = np.zeros(steps)
 CosineFlux[:] = np.cos((np.pi/Hc)*z[:])
-plt.figure(k)
-plt.plot(z,CosineFlux, 'k-')
-plt.grid()
 
-#plt.show()
