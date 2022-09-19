16901b71a07d8c4c440a5aaeee66c64eeeee05c4
Trevor <franklint2@vcu.edu>
2022-09-19 09:46:12 -0400

    changed TempBulkCalc from using hexdhcal to using the generic geometry file

--- TACOCAT/src/TempBulkCal.py
+++ TACOCAT/src/TempBulkCal.py
@@ -3,12 +3,18 @@
 
 import numpy as np
 import matplotlib as mpl
-import TACOCAT.src.HexDhCal
+from TACOCAT.src.Geometry_Value import Core_Geometry
 from scipy.integrate import trapz
+import os
+import sys
+sys.path.insert(0,'../..') #This adds the ability to geometry calculations from the main folder
+import TACOCAT_Read_In_File as TCinput
+
+Geometry_Type = TCinput.Geometry
 
 Tbulk = np.array([1,0])
 
-def Tbulk(z,FluxPro,Tbulkin,NFuel,qlin,Cp,Uinlet,rho,HaF):
+def Tbulk(z,FluxPro,Tbulkin,NFuel,qlin,Cp,Uinlet,rho):
 	for i in range(2,8):
-		Tbulk = Tbulkin + (np.array(trapz(z,None,i,0),FluxPro(np.array([0,i])))*NFuel*qlin)/(Cp*Uinlet*rho*HexDhCal.HaF(HexDhCal.Ha(Ac),NFuel,FoCD,WoD)) #Bulk Temperature of Coolant - C
+		Tbulk = Tbulkin + (np.array(trapz(z,None,i,0),FluxPro(np.array([0,i])))*NFuel*qlin)/(Cp*Uinlet*rho*Core_Geometry[Geometry_Type]["CoolantFlowArea"]) #Bulk Temperature of Coolant - C
 		return Tbulk
