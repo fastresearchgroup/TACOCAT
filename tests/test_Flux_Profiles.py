ffa2254e783abea1bf68e1cf1fa8cd374949ebff
Trevor <franklint2@vcu.edu>
2022-10-26 12:15:27 -0400

    chaning flux test to match testing structure of other test

--- tests/test_Flux_Profiles.py
+++ tests/test_Flux_Profiles.py
@@ -13,16 +13,16 @@ z = np.linspace(-Hc/2,Hc/2,steps)
 def test_FlatLineFlux():
     y = 1
     z_values = np.repeat(y,steps)
-    np.testing.assert_array_equal(z_values, fluxpro.FlatLineFlux[:])
+    assert list(z_values)==list(fluxpro.FlatLineFlux[:])
     
 def test_LinearFlux():
     z_values = z
-    np.testing.assert_array_equal(z_values, fluxpro.LinearFlux[:])
+    assert list(z_values)==list(fluxpro.LinearFlux[:])
 
 def test_ExponentialFlux():
     z_values = np.exp(z)
-    np.testing.assert_array_equal(z_values, fluxpro.ExponentialFlux[:])
+    assert list(z_values)==list(fluxpro.ExponentialFlux[:])
 
 def test_CosineFlux():
-    z_value = np.cos((np.pi/Hc)*z)
-    np.testing.assert_array_equal(z_value, fluxpro.CosineFlux[:])
+    z_values = np.cos((np.pi/Hc)*z)
+    assert list(z_values)==list(fluxpro.CosineFlux[:])
