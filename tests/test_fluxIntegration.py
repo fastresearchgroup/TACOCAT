574082a1badf439ed7cec64eb619e7e50473c2e5
Trevor <franklint2@vcu.edu>
2022-10-26 10:37:52 -0400

    adding documentation to code for clarification

--- tests/test_fluxIntegration.py
+++ tests/test_fluxIntegration.py
@@ -1,4 +1,5 @@
 #Trevor Franklin
+#BulkTemp integration test for straight line
 
 import pytest
 from pytest import approx
@@ -11,6 +12,7 @@ import TACOCAT_Read_In_File as TCinput
 from LoadedTACO.src.Geometry_Value import Core_Geometry
 from LoadedTACO.src.Coolant_Value import Coolant
 
+#Defining Constants
 steps = geometry.steps
 Hc = TCinput.Hc
 z = np.linspace(-Hc/2,Hc/2,steps)
@@ -18,24 +20,22 @@ Coolant_Type = TCinput.Coolant
 Geometry_Type = TCinput.Geometry
 qlin = TCinput.Qth/(geometry.NFuel*TCinput.Hc)
 
+#Defining Integration Constant 
 C = (geometry.NFuel*qlin)/(Coolant[Coolant_Type]["Cp"]*TCinput.Uinlet*Coolant[Coolant_Type]["rho"]*Core_Geometry[Geometry_Type]["CoolantFlowArea"])
 
+#Plotting known integration for straight line
 TestingTB = np.zeros(steps)
 TestingTB[:] = (1/2)*z[:]**2
 
+#Pulling BulkTemp plot from TACOCAT
 TempBulk = TC.BulkTemp(Flux_Profiles.LinearFlux)
 
+#Creating test bulk temp with known integration
 TestTB = np.zeros(steps)
 TestTB[0] = TCinput.Tbulkin
 for i in range(1,steps):
 	TestTB[i] = C*TestingTB[i]+TCinput.Tbulkin
 
-#np.testing.assert_allclose(TestTB, TempBulk, rtol=10)
+#Testing that the arrays are equal with an absolute tolerence of 10 degrees celsius
 def test_Temp_Bulk_Array():
 	assert list(TempBulk)==approx(list(TestTB), abs=10)
-
-'''
-plt.plot(z,TempBulk)
-plt.plot(x,ROC_TestTB)
-plt.show()
-'''
